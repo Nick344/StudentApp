0d3b1b95c513d6a5e70bf39d21c62e07f7303585
Nick344 <82670265+Nick344@users.noreply.github.com>
2024-05-28 01:38:04 +0300

    Add files via upload

---
+++ StudentApp/Models/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.NewFolder
+{
+    internal class Group
+    {
+
+        public int Id { get; set; }
+
+        public string GroupName { get; set; }
+
+        public ICollection<Student> Studebts { get; set; }
+    }
+}
